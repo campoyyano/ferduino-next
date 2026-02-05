01be4b0bf1f08206a6640f553ead7431bca038f6
Manolo <campoyyano@gmail.com>
2026-02-05 18:02:44 +0000

    Añadir HAL de placa (hal_board)

---
+++ Firmware/port/include/hal_board.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "board_config.h"
+
+namespace hal {
+
+  // HAL de placa: expone capacidades de la plataforma/board sin que
+  // el resto del código tenga que tocar macros directamente.
+
+  inline bool esPlaca5V() {
+    return boardEs5V;
+  }
+
+  inline bool esMega2560() {
+    return (BOARD_TARGET == BOARD_MEGA2560);
+  }
+
+  inline bool esDue() {
+    return (BOARD_TARGET == BOARD_DUE);
+  }
+
+  inline bool esGiga() {
+    return (BOARD_TARGET == BOARD_GIGA);
+  }
+
+  inline bool perfilLegacy() {
+    return (PINS_PROFILE == PINS_PROFILE_LEGACY);
+  }
+
+  inline bool perfilFase2() {
+    return (PINS_PROFILE == PINS_PROFILE_FASE2);
+  }
+
+}
